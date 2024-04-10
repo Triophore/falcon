9044c701ffef949acf8394f368f56505ca1c2d53
ls <venu@lifesignals.com>
2024-04-10 17:45:11 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # falcon
-Falcon is scalable , Rocket powered server framework
+Falcon is scalable , Rocket powered server framework written in rust
+The aim of this framework is make it as feature proof as possible
---
+++ src/main.rs
@@ -0,0 +1,143 @@
+#[macro_use] extern crate rocket;
+
+extern crate dotenv;
+use dotenv::dotenv;
+use rocket::{State, Route};
+use rocket::fs::{FileServer, relative};
+use rocket::http::ContentType;
+use rocket::request::FromRequest;
+use rocket::response::content::RawXml;
+use rocket_dyn_templates::{Template, tera::Tera, context};
+use rocket::response::{Responder, Response, content};
+use log::LevelFilter;
+use log::{error, info};
+use log4rs;
+use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
+use log4rs::append::rolling_file::policy::compound::{
+    roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger,
+};
+use log4rs::append::rolling_file::RollingFileAppender;
+use log4rs::config::{Appender, Logger, Root};
+use log4rs::Config;
+use log4rs::encode::pattern::PatternEncoder;
+use std::collections::HashMap;
+use std::{env};
+use reqwest::{Client, Url};
+use serde_json::{from_str, Value};
+use rocket::form::{Form};
+use rocket::form::FromFormField;
+use urlencoding::encode;
+
+use chrono::Utc;
+use chrono::format::strftime::StrftimeItems;
+
+
+
+
+
+
+
+
+
+//routes
+
+
+
+#[catch(404)]
+async fn not_found() -> Template {
+    let cms_url = env::var("CMS_HOST").unwrap_or(String::from("http://127.0.0.1:8090"));
+    let cms_public = env::var("CMS_PUBLIC").unwrap_or(String::from("http://127.0.0.1:8090"));
+    let mut context_data:  HashMap<String,Value> =  HashMap::new();
+    context_data.insert(String::from("cms_url"), serde_json::Value::String(cms_public.clone()));
+
+
+    let industries_url = format!("{}/api/collections/Industries/records?fields=Title",cms_url);
+    match reqwest::get(industries_url).await {
+         Ok(resp) => {
+             let json: serde_json::Value = resp.json().await.unwrap();
+             context_data.insert(String::from("industries"),json);
+           // info!("{:?}",context_data);
+         }
+         Err(err) => {
+            info!("Reqwest Error: {}", err)
+         }
+    }
+
+
+    Template::render("errors/404", context! {
+        context : context_data
+    })
+}
+
+
+//routes
+
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let log_line_pattern = "{d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {m}{n}";
+
+    let trigger_size = byte_unit::n_mb_bytes!(30) as u64;
+    let trigger = Box::new(SizeTrigger::new(trigger_size));
+
+    let roller_pattern = "logs/arch/log_{}.gz";
+    let roller_count = 5;
+    let roller_base = 1;
+    let roller = Box::new(
+        FixedWindowRoller::builder()
+            .base(roller_base)
+            .build(roller_pattern, roller_count)
+            .unwrap(),
+    );
+
+    let _compound_policy = Box::new(CompoundPolicy::new(trigger, roller));
+
+    let step_ap = RollingFileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(log_line_pattern)))
+        .build("logs/log/log.log", _compound_policy)
+        .unwrap();
+
+    //let stdout = ConsoleAppender::builder().build();
+
+    let config = Config::builder()
+        //.appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .appender(Appender::builder().build("step_ap", Box::new(step_ap)))
+        .logger(
+            Logger::builder()
+                .appender("step_ap")
+                .build("step", LevelFilter::Trace),
+        )
+
+        .build(
+            Root::builder()
+                .appender("step_ap")
+                .build(LevelFilter::Trace),
+        )
+        .unwrap();
+
+    let _handle = log4rs::init_config(config).unwrap();
+
+
+    dotenv().ok();
+    info!("---------------ENV----------------");
+    for (key, value) in env::vars() {
+        info!("{key}: {value}");
+    }
+    info!("---------------ENV----------------");
+
+    let _rocket = rocket::build()
+        .mount("/", routes![])
+        .mount("/static", FileServer::from(relative!("static")))
+        .attach(Template::fairing())
+        .register("/", catchers![not_found])
+        //.attach(Template::fairing(|_| {})) 
+        .launch()
+        .await?;
+
+
+        info!("{:?}", rocket::routes!());
+
+    Ok(())
+}
+
+
